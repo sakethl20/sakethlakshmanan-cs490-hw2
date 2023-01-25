8eca604081f6f4e8cd8bee4dcc2b0c7ed6f11849
Saketh Lakshmanan <saketh_lakshmanan@yahoo.com>
2023-01-25 14:05:36 -0500

    Chapter 1 changes

---
+++ redwoodblog/web/src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,15 @@
+import { MetaTags } from '@redwoodjs/web'
+
+const AboutPage = () => {
+  return (
+    <>
+      <MetaTags title="About" description="About page" />
+
+        <p>
+          This website was created to show my mastery of Redwood!
+        </p>
+    </>
+  )
+}
+
+export default AboutPage
